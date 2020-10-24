434c91a9c5884e9fd7f1397f106d985e9ebcc9fd
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-10-24 12:59:23 +0300

    add user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+
+  before_save { self.email = email.downcase }
+
+
+
+  validates_presence_of(:first_name, :last_name, :url, :email)
+
+
+  validates_length_of :email, maximum: 255
+
+  validates :email, uniqueness: { scope: :id }
 end
